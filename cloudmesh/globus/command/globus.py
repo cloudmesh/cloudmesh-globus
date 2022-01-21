3176ea96574a7b7ec5ac43ac58be15a3379b4d9b
Gregor von Laszewski <laszewski@gmail.com>
2022-01-21 10:29:10 -0500

    better globus with relative dirr

--- cloudmesh/globus/command/globus.py
+++ cloudmesh/globus/command/globus.py
@@ -1,6 +1,6 @@
 from cloudmesh.shell.command import command
 from cloudmesh.shell.command import PluginCommand
-from cloudmesh.globus.api.manager import Manager
+
 from cloudmesh.common.console import Console
 from cloudmesh.common.util import path_expand
 from pprint import pprint
@@ -16,7 +16,8 @@ class GlobusCommand(PluginCommand):
         ::
 
           Usage:
-                globus --file=FILE
+                globus login FROM_EP TO_EP
+                globus transfer FROM_EP TO_EP DIR FROM TO [--mkdir=no]
                 globus list
 
           This command does some useful things.
@@ -36,15 +37,52 @@ class GlobusCommand(PluginCommand):
 
         VERBOSE(arguments)
 
-        m = Manager()
+        from cloudmesh.globus.globus import Globus
 
-        if arguments.file:
-            print("option a")
-            m.list(path_expand(arguments.file))
+        if arguments.login or arguments.transfer:
+
+            from_ep = arguments.FROM_EP
+            to_ep = arguments.TO_EP
+
+            globus = Globus(source_endpoint=from_ep,
+                            destination_endpoint=to_ep)
+            globus.login(from_ep, to_ep)
+
+        if arguments.transfer:
+            "globus transfer FROM_EP TO_EP DIR FROM TO"
+
+            base_directory = arguments.DIR
+            from_dir = arguments.FROM
+            to_dir = arguments.TO
+
+            # SETUP LABEL
+            source = f"{from_ep}:{base_directory}/{from_dir}"
+            destination = f"{to_ep}:{to_dir}/{from_dir}"
+
+            char = "-"
+            label = f"{source}---{destination}"\
+                .replace("/", char)\
+                .replace(":", char)\
+                .replace(" ", char)\
+                .replace("_", char)\
+                .replace(".", char)
+
+            # SETUP ENDPOINTS
+            from_ep = globus.bookmark(from_ep)
+            to_ep = globus.bookmark(to_ep)
+
+            source = f"{from_ep}:{base_directory}/{from_dir}"
+            destination=f"{to_ep}:~{to_dir}/{from_dir}"
+
+
+
+            if arguments["--mkdir"] not in ["no", "NO", "0"]:
+                globus.mkdir(to_ep, f"/~{to_dir}")
+
+            globus.transfer(source, destination, label=label)
+
 
         elif arguments.list:
             print("option b")
-            m.list("just calling list without parameter")
 
-        Console.error("This is just a sample")
         return ""
